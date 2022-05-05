a3aef0ad45bfefd24847f946a731b19e49b98243
kirthigavadiveluk <104890409+kirthigavadiveluk@users.noreply.github.com>
2022-05-05 12:05:35 +0530

    Update HelloWorld.java

--- test/src/main/java/test/HelloWorld.java
+++ test/src/main/java/test/HelloWorld.java
@@ -3,7 +3,9 @@ package test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		String str="HelloWorld";
 		System.out.println("HelloWorld");
+		System.out.print(str);
 
 	}
 
