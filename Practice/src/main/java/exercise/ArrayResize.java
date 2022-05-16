9fa3a6a3ff78c840b88030ebc063c2e7a3a41af0
kirthiga.vadivelu <kirthiga.vadivelu@DT150117.QICHOAPPL.com>
2022-05-16 14:58:43 +0530

    16may

---
+++ Practice/src/main/java/exercise/ArrayResize.java
@@ -0,0 +1,23 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayResize {
+
+	public static void main(String[] args) {
+		int arr[] = new int[] { 10, 20, 30, 40, 50 };
+		for (int a : arr) {
+			System.out.println(a);
+		}
+		List<Integer> List=new ArrayList<Integer>();
+		for(int a:arr) {
+			List.add(a);
+		}
+			System.out.println(List);
+		List.add(100);
+		System.out.println(List);
+		
+	}
+
+}
