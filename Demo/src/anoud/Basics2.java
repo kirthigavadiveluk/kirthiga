cf4053123e329556b912f79dd09240d0a3c5b36c
kirthiga.vadivelu <kirthiga.vadivelu@DT150117.QICHOAPPL.com>
2022-05-18 11:40:37 +0530

    may

--- Demo/src/anoud/Basics2.java
+++ Demo/src/anoud/Basics2.java
@@ -9,6 +9,7 @@ public class Basics2 {
 		System.out.println("Enter the value of i");
 		int i = s1.nextInt();
 		System.out.println("Enter the value of j");
+		
 		int j = s1.nextInt();
 		int mul = i * j;
 		System.out.println("The multiplication of i and j is .." + mul);
