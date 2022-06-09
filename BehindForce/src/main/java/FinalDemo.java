e94657c714c2d93f65d1469f921975389ee2992a
kirthiga.vadivelu <kirthiga.vadivelu@DT150117.QICHOAPPL.com>
2022-06-09 12:50:48 +0530

    9-06

---
+++ BehindForce/src/main/java/FinalDemo.java
@@ -0,0 +1,31 @@
+
+public class FinalDemo {
+
+	public static void main(String[] args) {
+		VarTop1 vt=new VarTop1();
+		vt.top=20;//variable value cannot change
+	}
+
+}
+class VarTop1{
+	final int top=30;
+}
+class VarMed2 extends VarTop1{
+	  int top=20;
+	final public static void met() {
+		
+	}
+}
+class VarBot3 extends VarMed2{
+	 int top=10;
+	public static void met() {//method cannot be overridden
+		System.out.println();
+		
+	}
+}
+final class Findemo{
+	
+}
+class findemo extends Findemo{//class cannot  be inherit
+	
+}
